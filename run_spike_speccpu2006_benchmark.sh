94ba8d7a3e1a0dc84c8c93cb43a9469fc62cffff
Mark Gottscho <mgottscho@ucla.edu>
2016-08-12 15:36:34 -0700

    Updated scripts to use benchmark full codenames instead of just the suffix.

--- run_spike_speccpu2006_benchmark.sh
+++ run_spike_speccpu2006_benchmark.sh
@@ -24,7 +24,7 @@ if [[ "$ARGC" != 1 ]]; then # Bad number of arguments.
 	echo "This script runs a single gem5 simulation of a single SPEC CPU2006 benchmark on Spike for RISC-V."
 	echo ""
 	echo "USAGE: run_spike_speccpu2006_benchmark.sh <BENCHMARK>"
-	echo "EXAMPLE: ./run_spike_speccpu2006_benchmark.sh bzip2"
+	echo "EXAMPLE: ./run_spike_speccpu2006_benchmark.sh 401.bzip2"
 	echo ""
 	echo "A single --help help or -h argument will bring this message back."
 	exit
@@ -34,37 +34,37 @@ fi
 BENCHMARK=$1					# Benchmark name, e.g. bzip2
 
 ######################### BENCHMARK CODENAMES ####################
-PERLBENCH_CODE=400.perlbench
-BZIP2_CODE=401.bzip2
-GCC_CODE=403.gcc
-BWAVES_CODE=410.bwaves
-GAMESS_CODE=416.gamess
-MCF_CODE=429.mcf
-MILC_CODE=433.milc
-ZEUSMP_CODE=434.zeusmp
-GROMACS_CODE=435.gromacs
-CACTUSADM_CODE=436.cactusADM
-LESLIE3D_CODE=437.leslie3d
-NAMD_CODE=444.namd
-GOBMK_CODE=445.gobmk
-DEALII_CODE=447.dealII
-SOPLEX_CODE=450.soplex
-POVRAY_CODE=453.povray
-CALCULIX_CODE=454.calculix
-HMMER_CODE=456.hmmer
-SJENG_CODE=458.sjeng
-GEMSFDTD_CODE=459.GemsFDTD
-LIBQUANTUM_CODE=462.libquantum
-H264REF_CODE=464.h264ref
-TONTO_CODE=465.tonto
-LBM_CODE=470.lbm
-OMNETPP_CODE=471.omnetpp
-ASTAR_CODE=473.astar
-WRF_CODE=481.wrf
-SPHINX3_CODE=482.sphinx3
-XALANCBMK_CODE=483.xalancbmk
-SPECRAND_INT_CODE=998.specrand
-SPECRAND_FLOAT_CODE=999.specrand
+PERLBENCH=400.perlbench
+BZIP2=401.bzip2
+GCC=403.gcc
+BWAVES=410.bwaves
+GAMESS=416.gamess
+MCF=429.mcf
+MILC=433.milc
+ZEUSMP=434.zeusmp
+GROMACS=435.gromacs
+CACTUSADM=436.cactusADM
+LESLIE3D=437.leslie3d
+NAMD=444.namd
+GOBMK=445.gobmk
+DEALII=447.dealII
+SOPLEX=450.soplex
+POVRAY=453.povray
+CALCULIX=454.calculix
+HMMER=456.hmmer
+SJENG=458.sjeng
+GEMSFDTD=459.GemsFDTD
+LIBQUANTUM=462.libquantum
+H264REF=464.h264ref
+TONTO=465.tonto
+LBM=470.lbm
+OMNETPP=471.omnetpp
+ASTAR=473.astar
+WRF=481.wrf
+SPHINX3=482.sphinx3
+XALANCBMK=483.xalancbmk
+SPECRAND_INT=998.specrand
+SPECRAND_FLOAT=999.specrand
 ##################################################################
 
 ######################### BENCHMARK INPUTS ## ####################
@@ -105,142 +105,107 @@ SPECRAND_FLOAT_ARGS="1255432124 234923"
 
 # Check BENCHMARK input
 #################### BENCHMARK CODE MAPPING ######################
-BENCHMARK_CODE="none"
-
-if [[ "$BENCHMARK" == "perlbench" ]]; then
-	BENCHMARK_CODE=$PERLBENCH_CODE
+if [[ "$BENCHMARK" == "$PERLBENCH" ]]; then
 	BENCHMARK_ARGS=$PERLBENCH_ARGS
 fi
-if [[ "$BENCHMARK" == "bzip2" ]]; then
-	BENCHMARK_CODE=$BZIP2_CODE
+if [[ "$BENCHMARK" == "$BZIP2" ]]; then
 	BENCHMARK_ARGS=$BZIP2_ARGS
 fi
-if [[ "$BENCHMARK" == "gcc" ]]; then
-	BENCHMARK_CODE=$GCC_CODE
+if [[ "$BENCHMARK" == "$GCC" ]]; then
 	BENCHMARK_ARGS=$GCC_ARGS
 fi
-if [[ "$BENCHMARK" == "bwaves" ]]; then
-	BENCHMARK_CODE=$BWAVES_CODE
+if [[ "$BENCHMARK" == "$BWAVES" ]]; then
 	BENCHMARK_ARGS=$BWAVES_ARGS
 fi
-if [[ "$BENCHMARK" == "gamess" ]]; then
-	BENCHMARK_CODE=$GAMESS_CODE
+if [[ "$BENCHMARK" == "$GAMESS" ]]; then
 	BENCHMARK_ARGS=$GAMESS_ARGS
 fi
-if [[ "$BENCHMARK" == "mcf" ]]; then
-	BENCHMARK_CODE=$MCF_CODE
+if [[ "$BENCHMARK" == "$MCF" ]]; then
 	BENCHMARK_ARGS=$MCF_ARGS
 fi
-if [[ "$BENCHMARK" == "milc" ]]; then
-	BENCHMARK_CODE=$MILC_CODE
+if [[ "$BENCHMARK" == "$MILC" ]]; then
 	BENCHMARK_ARGS=$MILC_ARGS
 fi
-if [[ "$BENCHMARK" == "zeusmp" ]]; then
-	BENCHMARK_CODE=$ZEUSMP_CODE
+if [[ "$BENCHMARK" == "$ZEUSMP" ]]; then
 	BENCHMARK_ARGS=$ZEUSMP_ARGS
 fi
-if [[ "$BENCHMARK" == "gromacs" ]]; then
-	BENCHMARK_CODE=$GROMACS_CODE
+if [[ "$BENCHMARK" == "$GROMACS" ]]; then
 	BENCHMARK_ARGS=$GROMACS_ARGS
 fi
-if [[ "$BENCHMARK" == "cactusADM" ]]; then
-	BENCHMARK_CODE=$CACTUSADM_CODE
+if [[ "$BENCHMARK" == "$CACTUSADM" ]]; then
 	BENCHMARK_ARGS=$CACTUSADM_ARGS
 fi
-if [[ "$BENCHMARK" == "leslie3d" ]]; then
-	BENCHMARK_CODE=$LESLIE3D_CODE
+if [[ "$BENCHMARK" == "$LESLIE3D" ]]; then
 	BENCHMARK_ARGS=$LESLIE3D_ARGS
 fi
-if [[ "$BENCHMARK" == "namd" ]]; then
-	BENCHMARK_CODE=$NAMD_CODE
+if [[ "$BENCHMARK" == "$NAMD" ]]; then
 	BENCHMARK_ARGS=$NAMD_ARGS
 fi
-if [[ "$BENCHMARK" == "gobmk" ]]; then
-	BENCHMARK_CODE=$GOBMK_CODE
+if [[ "$BENCHMARK" == "$GOBMK" ]]; then
 	BENCHMARK_ARGS=$GOBMK_ARGS
 fi
-if [[ "$BENCHMARK" == "dealII" ]]; then # DOES NOT WORK
-	BENCHMARK_CODE=$DEALII_CODE
+if [[ "$BENCHMARK" == "$DEALII" ]]; then # DOES NOT WORK
 	BENCHMARK_ARGS=$DEALII_ARGS
 fi
-if [[ "$BENCHMARK" == "soplex" ]]; then
-	BENCHMARK_CODE=$SOPLEX_CODE
+if [[ "$BENCHMARK" == "$SOPLEX" ]]; then
 	BENCHMARK_ARGS=$SOPLEX_ARGS
 fi
-if [[ "$BENCHMARK" == "povray" ]]; then
-	BENCHMARK_CODE=$POVRAY_CODE
+if [[ "$BENCHMARK" == "$POVRAY" ]]; then
 	BENCHMARK_ARGS=$POVRAY_ARGS
 fi
-if [[ "$BENCHMARK" == "calculix" ]]; then
-	BENCHMARK_CODE=$CALCULIX_CODE
+if [[ "$BENCHMARK" == "$CALCULIX" ]]; then
 	BENCHMARK_ARGS=$CALCULIX_ARGS
 fi
-if [[ "$BENCHMARK" == "hmmer" ]]; then
-	BENCHMARK_CODE=$HMMER_CODE
+if [[ "$BENCHMARK" == "$HMMER" ]]; then
 	BENCHMARK_ARGS=$HMMER_ARGS
 fi
-if [[ "$BENCHMARK" == "sjeng" ]]; then
-	BENCHMARK_CODE=$SJENG_CODE
+if [[ "$BENCHMARK" == "$SJENG" ]]; then
 	BENCHMARK_ARGS=$SJENG_ARGS
 fi
-if [[ "$BENCHMARK" == "GemsFDTD" ]]; then
-	BENCHMARK_CODE=$GEMSFDTD_CODE
+if [[ "$BENCHMARK" == "$GEMSFDTD" ]]; then
 	BENCHMARK_ARGS=$GEMSFDTD_ARGS
 fi
-if [[ "$BENCHMARK" == "libquantum" ]]; then
-	BENCHMARK_CODE=$LIBQUANTUM_CODE
+if [[ "$BENCHMARK" == "$LIBQUANTUM" ]]; then
 	BENCHMARK_ARGS=$LIBQUANTUM_ARGS
 fi
-if [[ "$BENCHMARK" == "h264ref" ]]; then
-	BENCHMARK_CODE=$H264REF_CODE
+if [[ "$BENCHMARK" == "$H264REF" ]]; then
 	BENCHMARK_ARGS=$H264REF_ARGS
 fi
-if [[ "$BENCHMARK" == "tonto" ]]; then
-	BENCHMARK_CODE=$TONTO_CODE
+if [[ "$BENCHMARK" == "$TONTO" ]]; then
 	BENCHMARK_ARGS=$TONTO_ARGS
 fi
-if [[ "$BENCHMARK" == "lbm" ]]; then
-	BENCHMARK_CODE=$LBM_CODE
+if [[ "$BENCHMARK" == "$LBM" ]]; then
 	BENCHMARK_ARGS=$LBM_ARGS
 fi
-if [[ "$BENCHMARK" == "omnetpp" ]]; then
-	BENCHMARK_CODE=$OMNETPP_CODE
+if [[ "$BENCHMARK" == "$OMNETPP" ]]; then
 	BENCHMARK_ARGS=$OMNETPP_ARGS
 fi
-if [[ "$BENCHMARK" == "astar" ]]; then
-	BENCHMARK_CODE=$ASTAR_CODE
+if [[ "$BENCHMARK" == "$ASTAR" ]]; then
 	BENCHMARK_ARGS=$ASTAR_ARGS
 fi
-if [[ "$BENCHMARK" == "wrf" ]]; then
-	BENCHMARK_CODE=$WRF_CODE
+if [[ "$BENCHMARK" == "$WRF" ]]; then
 	BENCHMARK_ARGS=$WRF_ARGS
 fi
-if [[ "$BENCHMARK" == "sphinx3" ]]; then
-	BENCHMARK_CODE=$SPHINX3_CODE
+if [[ "$BENCHMARK" == "SPHINX3" ]]; then
 	BENCHMARK_ARGS=$SPHINX3_ARGS
 fi
-if [[ "$BENCHMARK" == "xalancbmk" ]]; then # DOES NOT WORK
-	BENCHMARK_CODE=$XALANCBMK_CODE
+if [[ "$BENCHMARK" == "$XALANCBMK" ]]; then # DOES NOT WORK
 	BENCHMARK_ARGS=$XALANCBMK_ARGS
 fi
-if [[ "$BENCHMARK" == "specrand_i" ]]; then
-	BENCHMARK_CODE=$SPECRAND_INT_CODE
+if [[ "$BENCHMARK" == "$SPECRAND_INT" ]]; then
 	BENCHMARK_ARGS=$SPECRAND_INT_ARGS
 fi
-if [[ "$BENCHMARK" == "specrand_f" ]]; then
-	BENCHMARK_CODE=$SPECRAND_FLOAT_CODE
+if [[ "$BENCHMARK" == "$SPECRAND_FLOAT" ]]; then
 	BENCHMARK_ARGS=$SPECRAND_FLOAT_ARGS
 fi
 
-# Sanity check
-if [[ "$BENCHMARK_CODE" == "none" ]]; then
-	echo "Input benchmark selection $BENCHMARK did not match any known SPEC CPU2006 benchmarks! Exiting."
-	exit 1
-fi
+# Extract just the part after the numeric code in the beginning of the benchmark name, e.g. 401.bzip2 --> bzip2
+BENCHMARK_NAME=`echo $BENCHMARK | sed 's/[0-9]*\.\([a-z0-9]*\)/\1/'`
+
 ##################################################################
 
 SPEC_CONFIG_SUFFIX=$SYSTEM-rv64g-priv-1.7-stable
-RUN_DIR=$SPEC_DIR/benchspec/CPU2006/$BENCHMARK_CODE/run/run_base_ref_${SPEC_CONFIG_SUFFIX}.0000		# Run directory for the selected SPEC benchmark
+RUN_DIR=$SPEC_DIR/benchspec/CPU2006/$BENCHMARK/run/run_base_ref_${SPEC_CONFIG_SUFFIX}.0000		# Run directory for the selected SPEC benchmark
 
 #################### LAUNCH SIMULATION ######################
 echo ""
@@ -254,9 +219,8 @@ echo ""
 echo ""
 
 # Actually launch spike.
-#CMD="$SPIKE_DIR/spike --randmemdatatrace=1000000:$OUTPUT_DIR/spike_mem_data_trace_${BENCHMARK}.txt --memwordsize=8 --ic=1:1:64 --dc=1:1:64 --isa=RV64G -m1024 -p1 $RISCV/riscv64-unknown-elf/bin/pk ${BENCHMARK}_base.${SPEC_CONFIG_SUFFIX} $BENCHMARK_ARGS"
-CMD="$SPIKE_DIR/spike --faultinj=10:inst:/u/home/m/mgottsch/project-puneet/eccgrp-ecc-ctrl/inst_recovery_spike_wrapper.sh --memwordsize=8 --ic=1:1:64 --dc=1:1:64 --isa=RV64G -m1024 -p1 $RISCV/riscv64-unknown-elf/bin/pk ${BENCHMARK}_base.${SPEC_CONFIG_SUFFIX} $BENCHMARK_ARGS"
-#CMD="$SPIKE_DIR/spike --memwordsize=8 --ic=1:1:64 --dc=1:1:64 --isa=RV64G -m1024 -p1 $RISCV/riscv64-unknown-elf/bin/pk ${BENCHMARK}_base.${SPEC_CONFIG_SUFFIX} $BENCHMARK_ARGS"
+#CMD="$SPIKE_DIR/spike --randmemdatatrace=1000000:$OUTPUT_DIR/spike_mem_data_trace_${BENCHMARK}.txt --memwordsize=8 --ic=1:1:64 --dc=1:1:64 --isa=RV64G -m1024 -p1 $RISCV/riscv64-unknown-elf/bin/pk ${BENCHMARK_NAME}_base.${SPEC_CONFIG_SUFFIX} $BENCHMARK_ARGS"
+CMD="$SPIKE_DIR/spike --faultinj=10:inst:/u/home/m/mgottsch/project-puneet/eccgrp-ecc-ctrl/inst_recovery_spike_wrapper.sh --memwordsize=8 --ic=1:1:64 --dc=1:1:64 --isa=RV64G -m1024 -p1 $RISCV/riscv64-unknown-elf/bin/pk ${BENCHMARK_NAME}_base.${SPEC_CONFIG_SUFFIX} $BENCHMARK_ARGS"
 echo $CMD
 $CMD
 
