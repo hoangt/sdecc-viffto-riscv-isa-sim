364dc5e293c8d384d7597f7d3e7f70bf30533627
Mark Gottscho <mgottscho@ucla.edu>
2017-01-30 20:29:28 -0800

    Added minimal example of a Hello World program for RISC-V that can register its own user-level callback function for our own newly-defined memory DUE trap. Requires custom Spike support by adding a new trap. Also requires riscv-pk (proxy kernel) support to implement a new environment call to allow user code to register the callback function. Finally we also require pk support for implementing the default DUE behavior, which is to panic

--- hello.c
+++ hello.c
@@ -7,11 +7,32 @@
 
 #include <stdio.h>
 
-void handle_memory_due() {
-    asm volatile("lui a0, 0xdead");
+// Originally defined in riscv-pk/include/pk.h
+typedef struct {
+    long gpr[32];
+    long status;
+    long epc;
+    long badvaddr;
+    long cause;
+    long insn;
+} trapframe_t;
+
+typedef void (*user_trap_handler)(trapframe_t* tf); //MWG added to riscv-pk/include/pk.h
+
+void my_due_handler(trapframe_t* tf) {
+    asm volatile("lui a0, 0xdead;"); //Write a canary value to register a0 that we can breakpoint on to make sure this actually gets executed
+}
+
+void register_user_memory_due_trap_handler(user_trap_handler fptr) {
+    asm volatile("or a0, zero, %0;" //Load user trap handler fptr into register a0
+                 "li a7, 447;" //Load syscall number 447 (SYS_register_user_memory_due_trap_handler) into register a7
+                 "ecall;" //Make RISC-V environment call to register our user-defined trap handler
+                 :
+                 : "r" (fptr));
 }
 
 int main(int argc, char** argv) {
+    register_user_memory_due_trap_handler(&my_due_handler);
     for (volatile unsigned long i = 0; i < 100000000; i++);
     printf("Hello World!\n");
     return 0;
