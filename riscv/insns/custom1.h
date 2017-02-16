385c172058d358ce9f0b7551c679824d89e0eb7c
Mark Gottscho <mgottscho@ucla.edu>
2017-02-15 17:47:53 -0800

    bugfixes

--- riscv/insns/custom1.h
+++ riscv/insns/custom1.h
@@ -2,6 +2,7 @@ if (MMU.errInjEnabled()) {
     MMU.err_inj_target_ = "data";
     MMU.err_inj_enable_ = true;
     MMU.err_inj_step_ = total_steps+1;
+    std::cout << std::dec;
     std::cout << "ERROR INJECTION ENABLED for DATA MEMORY by application, effective on next step: " << total_steps+1 << "!" << std::endl;
 } else {
     std::cout << "ERROR: Application tried to arm error injection for data memory, but Spike was not started with the error injection mode." << std::endl;
--- riscv/insns/custom3_rs1.h
+++ riscv/insns/custom3_rs1.h
@@ -1,5 +1,9 @@
-reg_t msg = p->get_csr(CSR_PENALTY_BOX_MSG);
+reg_t msg;
 reg_t cl[8];
+reg_t blockpos;
+
+//Read CSRs
+msg = p->get_csr(CSR_PENALTY_BOX_MSG);
 cl[0] = p->get_csr(CSR_PENALTY_BOX_CACHELINE_BLK0);
 cl[1] = p->get_csr(CSR_PENALTY_BOX_CACHELINE_BLK1);
 cl[2] = p->get_csr(CSR_PENALTY_BOX_CACHELINE_BLK2);
@@ -8,15 +12,24 @@ cl[4] = p->get_csr(CSR_PENALTY_BOX_CACHELINE_BLK4);
 cl[5] = p->get_csr(CSR_PENALTY_BOX_CACHELINE_BLK5);
 cl[6] = p->get_csr(CSR_PENALTY_BOX_CACHELINE_BLK6);
 cl[7] = p->get_csr(CSR_PENALTY_BOX_CACHELINE_BLK7);
-reg_t blockpos = p->get_csr(CSR_PENALTY_BOX_CACHELINE_BLKPOS);
-uint8_t victim_message[8];
-memcpy(victim_message, &msg, 8);
+blockpos = p->get_csr(CSR_PENALTY_BOX_CACHELINE_BLKPOS);
+
+//Convert values to lumps of bytes
+uint8_t* victim_message = reinterpret_cast<uint8_t*>(&msg);
 uint8_t cacheline[64];
+//memcpy(victim_message, &msg, 8);
 memcpy(cacheline, cl, 64);
+
+//Run external command
 std::string script_filename = "/home/mgottscho/Git/eccgrp-ecc-ctrl/data_recovery_spike_wrapper.sh";
 std::string cmd = construct_sdecc_recovery_cmd(script_filename, victim_message, 8, cacheline, (unsigned)(blockpos));
 std::string output = myexec(cmd);
-std::cout << output << std::endl;
 const char* output_cstr = output.c_str();
-//memcpy((void*)RS1, output_cstr, output.length());
+
 MMU.store_slow_path(RS1, output.length(), (const uint8_t*)output_cstr, 0);
+
+std::cout << " Original message: ";
+for (size_t i = 0; i < 8; i++)
+  std::cout << std::bitset<8>(victim_message[i]);
+std::cout << std::endl;
+std::cout << "Recovered message: " << output; //output has newline built in
