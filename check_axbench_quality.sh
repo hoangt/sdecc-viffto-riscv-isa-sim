239f9b9f4c386fbb3e31f5c95a4024f2a9cba53c
Mark Gottscho <mgottscho@ucla.edu>
2017-03-05 23:42:12 -0800

    Fixed script

--- check_axbench_quality.sh
+++ check_axbench_quality.sh
@@ -24,32 +24,32 @@ QOS_SCRIPT=$AXBENCH_DIR/applications/$AXBENCH_BENCHMARK/scripts/qos.py
 SAMPLES_DATA=`ls $TEST_DIR | grep -e "${AXBENCH_BENCHMARK}\\.[0-9]*\\.data"`
 SEQNUMS=`echo "$SAMPLES_DATA" | sed -r 's/[a-z0-9]*\\.([0-9]*)\\.data/\1/'`
 
-#for SEQNUM in $SEQNUMS
-#do
-#    echo "Sample $SEQNUM..."
-#    SAMPLE_DATA=${AXBENCH_BENCHMARK}.$SEQNUM.data
-#    SAMPLE_STDOUT=${AXBENCH_BENCHMARK}.$SEQNUM.stdout
-#    SAMPLE_QOS=${AXBENCH_BENCHMARK}.$SEQNUM.qos
-#    PANICKED=`grep -l "FAILED DUE RECOVERY" $TEST_DIR/$SAMPLE_STDOUT`
-#    if [[ "$PANICKED" == "$TEST_DIR/$SAMPLE_STDOUT" ]]; then
-#        echo "PANICKED" > $TEST_DIR/$SAMPLE_QOS
-#    else
-#        CRASHED=`grep -l "PANIC" $TEST_DIR/$SAMPLE_STDOUT`
-#        if [[ "$CRASHED" == "$TEST_DIR/$SAMPLE_STDOUT" ]]; then
-#            echo "CRASHED" > $TEST_DIR/$SAMPLE_QOS
-#        else 
-#            SUCCESS=`grep -l "SUCCESS" $TEST_DIR/$SAMPLE_STDOUT`
-#            if [[ "$SUCCESS" == "$TEST_DIR/$SAMPLE_STDOUT" ]]; then
-#                ERROR_RAW=`python $QOS_SCRIPT $GOLDEN ${TEST_DIR}/${SAMPLE_DATA}`
-#                ERROR=`echo "$ERROR_RAW" | sed -r 's/\*\*\* Error: (-?[0-9]\\.[0-9]*)/\1/'`
-#                echo $ERROR > $TEST_DIR/$SAMPLE_QOS
-#            else
-#                echo "QOSFAIL" > $TEST_DIR/$SAMPLE_QOS
-#                echo "$SEQNUM had QoS fail"
-#            fi
-#        fi
-#    fi
-#done
+for SEQNUM in $SEQNUMS
+do
+    echo "Sample $SEQNUM..."
+    SAMPLE_DATA=${AXBENCH_BENCHMARK}.$SEQNUM.data
+    SAMPLE_STDOUT=${AXBENCH_BENCHMARK}.$SEQNUM.stdout
+    SAMPLE_QOS=${AXBENCH_BENCHMARK}.$SEQNUM.qos
+    PANICKED=`grep -l "FAILED DUE RECOVERY" $TEST_DIR/$SAMPLE_STDOUT`
+    if [[ "$PANICKED" == "$TEST_DIR/$SAMPLE_STDOUT" ]]; then
+        echo "PANICKED" > $TEST_DIR/$SAMPLE_QOS
+    else
+        CRASHED=`grep -l "PANIC" $TEST_DIR/$SAMPLE_STDOUT`
+        if [[ "$CRASHED" == "$TEST_DIR/$SAMPLE_STDOUT" ]]; then
+            echo "CRASHED" > $TEST_DIR/$SAMPLE_QOS
+        else 
+            SUCCESS=`grep -l "SUCCESS" $TEST_DIR/$SAMPLE_STDOUT`
+            if [[ "$SUCCESS" == "$TEST_DIR/$SAMPLE_STDOUT" ]]; then
+                ERROR_RAW=`python $QOS_SCRIPT $GOLDEN ${TEST_DIR}/${SAMPLE_DATA}`
+                ERROR=`echo "$ERROR_RAW" | sed -r 's/\*\*\* Error: (-?[0-9]\\.[0-9]*)/\1/'`
+                echo $ERROR > $TEST_DIR/$SAMPLE_QOS
+            else
+                echo "QOSFAIL" > $TEST_DIR/$SAMPLE_QOS
+                echo "$SEQNUM had QoS fail"
+            fi
+        fi
+    fi
+done
 
 cat $TEST_DIR/*qos | grep "PANICKED" | wc -l > $TEST_DIR/panics.txt
 cat $TEST_DIR/*qos | grep "CRASHED" | wc -l > $TEST_DIR/crashes.txt
