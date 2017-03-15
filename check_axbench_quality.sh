ac42020b1f4f585e0bce4ac13072a69ec3abc2fb
Mark Gottscho <mgottscho@ucla.edu>
2017-03-14 20:41:19 -0700

    bugfix on check_axbench_quality.sh

--- check_axbench_quality.sh
+++ check_axbench_quality.sh
@@ -120,12 +120,12 @@ cat $TEST_DIR/*qos | grep "PANICKED" > $TEST_DIR/panics.txt
 cat $TEST_DIR/*qos | grep -e "([A-Z]*)" > $TEST_DIR/recovered.txt
 
 cat $TEST_DIR/*qos | grep -e "[0-9]*\.[0-9]* (CORRECT)" > $TEST_DIR/correct.txt
-cat $TEST_DIR/correct.txt | grep -e "$GOLDEN_ERROR (CORRECT)" | sed -r "s/$GOLDEN_ERROR/0*\.0*/g" > $TEST_DIR/recovered_correct.txt
+cat $TEST_DIR/correct.txt | grep -e "$GOLDEN_ERROR (CORRECT)" | sed -r "s/$GOLDEN_ERROR/0\.00000000/g" > $TEST_DIR/recovered_correct.txt
 cat $TEST_DIR/recovered_correct.txt | sed -r 's/ \(CORRECT\)//g' > $TEST_DIR/recovered_correct.csv
 cat $TEST_DIR/correct.txt | grep -v -e "$GOLDEN_ERROR (CORRECT)" > $TEST_DIR/recovered_correct_but_error.txt
 
 cat $TEST_DIR/*qos | grep -e "(MCE)" > $TEST_DIR/mce.txt
-cat $TEST_DIR/mce.txt | grep -e "$GOLDEN_ERROR (MCE)" | sed -r "s/$GOLDEN_ERROR/0*\.0*/g" > $TEST_DIR/recovered_benign.txt
+cat $TEST_DIR/mce.txt | grep -e "$GOLDEN_ERROR (MCE)" | sed -r "s/$GOLDEN_ERROR/0\.00000000/g" > $TEST_DIR/recovered_benign.txt
 cat $TEST_DIR/recovered_benign.txt | sed -r 's/ \(MCE\)//g' > $TEST_DIR/recovered_benign.csv
 cat $TEST_DIR/mce.txt | grep "CRASHED" > $TEST_DIR/recovered_crashes.txt
 cat $TEST_DIR/mce.txt | grep -v -e "CRASHED" | grep -v -e "HANG (MCE)" | grep -v -e "$GOLDEN_ERROR (MCE)" > $TEST_DIR/recovered_sdc.txt
